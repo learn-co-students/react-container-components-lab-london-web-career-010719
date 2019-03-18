54b2d46bb0e5aedade810a42f950767270b6841e
Chris Camacho <cacamacho91@gmail.com>
2019-03-18 11:44:19 +0000

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,45 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
+import React, { Component } from 'react'
+import { Input, Button } from 'semantic-ui-react'
+import 'isomorphic-fetch'
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'U4oI1RWooo5rsSXrscLyOI4oleKxuvqG'
+const BASE_URL =
+  'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' +
+  `api-key=${NYT_API_KEY}&query=`
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+  state = {
+    reviews: [],
+    searchTerm: ''
+  }
+
+  handleSearchChangeInput = event =>
+    this.setState({ searchTerm: event.target.value })
+
+  handleClick = event => {
+    event.preventDefault()
+    fetch(BASE_URL.concat(this.state.searchTerm))
+      .then(resp => resp.json())
+      .then(respObject => this.setState({ reviews: respObject.results }))
+  }
+
+  render() {
+    return (
+      <div className='searchable-movie-reviews'>
+        <Input
+          value={this.state.searchTerm}
+          onChange={this.handleSearchChangeInput}
+          placeholder='Search for a review...'
+        />
+        <Button onClick={this.handleClick} primary>
+          Search
+        </Button>
+        <MovieReviews reviews={this.state.reviews} />
+        <hr />
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer
